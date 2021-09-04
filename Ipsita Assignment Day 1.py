958d61f90aec1589a291bc7b41a8d9e5af4488a4
Ipsita45 <90100588+Ipsita45@users.noreply.github.com>
2021-09-04 19:55:46 +0530

    Ipsita Barari Day1 Assignment Submitted

---
+++ Ipsita Assignment Day 1.py
@@ -0,0 +1,24 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 25
+>>> b = 35
+>>> a+b
+60
+>>> A = 66
+>>> B = "87"
+>>> A+B
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    A+B
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> Marks = 86
+>>> Name = "Ipsita"
+>>> Age = 23
+>>> print (f"The marks of {Name} is {Marks} and age is {Age}")
+The marks of Ipsita is 86 and age is 23
+>>> print ("Hello\t World")
+Hello	 World
+>>> print ("Hello\n World")
+Hello
+ World
+>>> 
